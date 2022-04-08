0b8ed7af68a3dd4a0ae9a5c81e17014402df4fab
Nas <johncarter786001@gmail.com>
2022-04-08 11:58:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-//document.getElementById("count-el").innerText = 5;
-
-//  let counti = 5;
-//  console.log(counti)
-//  console.log(typeof (counti))
-
-// let myAge = 33;
-// let humanDogRatio =7;
-// let myDogAge = myAge * humanDogRatio;
-// console.log(myDogAge)
-
-// let bonusPoints = 50;
-// console.log(bonusPoints)
-// bonusPoints = bonusPoints + 50;
-// console.log(bonusPoints)
-// bonusPoints = bonusPoints - 75;
-// console.log(bonusPoints)
-// bonusPoints = bonusPoints + 45;
-// console.log(bonusPoints)
-
-
-
-
-
-
 
 
  let countEl = document.getElementById("count-el");
